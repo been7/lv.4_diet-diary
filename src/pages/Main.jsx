8bb4571c31f5bb8469118fd3433cb4153282fa3e
Been7 <qlsl707@hanmail.net>
2023-09-08 23:08:53 +0900

    Chore. og태그 테스트

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -6,12 +6,20 @@ import Home from "../components/Main/Home";
 function Main() {
   return (
     <>
+      <title>식단일기</title>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <meta
+        name="description"
+        content="Web site created using create-react-app"
+      />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://lv-4-diet-diary.vercel.app/" />
       <meta property="og:title" content="Lv.4" />
       <meta
         property="og:image"
-        content="https://pbs.twimg.com/media/FIbhNioakAAc_V0?format=jpg&name=large"
+        content="https://qqmpajgfevwczsrkmsyu.supabase.co/storage/v1/object/public/countryImgs/Hong%20Kong.jpg?t=2023-09-08T12%3A18%3A05.237Z"
       />
       <meta property="og:description" content="테스트용 설명..." />
       <meta property="og:site_name" content="Lv.4 과제" />
