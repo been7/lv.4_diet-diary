5c1088b31d77fa675add148dd70db440b4c4aa7e
Been7 <qlsl707@hanmail.net>
2023-09-09 01:01:21 +0900

    Chore. og태그 테스트

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -8,18 +8,18 @@ function List() {
   return (
     <>
       <Helmet>
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://lv-4-diet-diary.vercel.app/" />
-        <meta property="og:title" content="Lv.4 목록이다" />
+        <meta property="otype" content="website" />
+        <meta property="url" content="https://lv-4-diet-diary.vercel.app/" />
+        <meta property="title" content="Lv.4 목록이다" />
         <meta
-          property="og:image"
+          property="image"
           content="https://pbs.twimg.com/media/FIbhNioakAAc_V0?format=jpg&name=large"
         />
-        <meta property="og:description" content="여긴 목록" />
-        <meta property="og:site_name" content="Lv.4 과제" />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
+        <meta property="description" content="여긴 목록" />
+        <meta property="site_name" content="Lv.4 과제" />
+        <meta property="locale" content="en_US" />
+        <meta property="image:width" content="1200" />
+        <meta property="image:height" content="630" />
       </Helmet>
       <Header />
       <Layout>
