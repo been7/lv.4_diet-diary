dce09e908b148aa3cbd2538588b968dba0b21875
Been7 <qlsl707@hanmail.net>
2023-09-08 23:02:04 +0900

    Chore. og태그 테스트

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import DietList from "../components/DietList/DietList";
 import Header from "../components/Header/Header";
 import Layout from "../components/Layout/Layout";
-import DietList from "../components/DietList/DietList";
 
 function List() {
   return (
     <>
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://lv-4-diet-diary.vercel.app/" />
+      <meta property="og:title" content="Lv.4 목록이다" />
+      <meta
+        property="og:image"
+        content="https://pbs.twimg.com/media/FIbhNioakAAc_V0?format=jpg&name=large"
+      />
+      <meta property="og:description" content="여긴 목록" />
+      <meta property="og:site_name" content="Lv.4 과제" />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
       <Header />
       <Layout>
         <DietList />
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-import Layout from "../components/Layout/Layout";
 import Header from "../components/Header/Header";
+import Layout from "../components/Layout/Layout";
 import Home from "../components/Main/Home";
 
 function Main() {
   return (
     <>
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://lv-4-diet-diary.vercel.app/" />
+      <meta property="og:title" content="Lv.4" />
+      <meta
+        property="og:image"
+        content="https://pbs.twimg.com/media/FIbhNioakAAc_V0?format=jpg&name=large"
+      />
+      <meta property="og:description" content="테스트용 설명..." />
+      <meta property="og:site_name" content="Lv.4 과제" />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
       <Header />
       <Layout>
         <Home />
